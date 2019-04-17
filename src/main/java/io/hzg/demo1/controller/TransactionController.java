9e710564453d6b9efed94fde1785656641a8ca4d
1464770973 <1464770973@qq.com>
2019-04-17 19:49:39 +0800

    add transactioncontroller

---
+++ src/main/java/io/hzg/demo1/controller/TransactionController.java
@@ -0,0 +1,34 @@
+package io.hzg.demo1.controller;
+
+import io.hzg.demo1.dto.TransactionInfoDTO;
+import io.hzg.demo1.dto.TransactionListDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    @GetMapping("/getRecentTransactionsById")
+    public List<TransactionListDTO> getRecentTransactionsById(@RequestParam Integer blockchainId){
+        return null;
+    }
+    @GetMapping("/getRecentTransactionsByNameType")
+    public List<TransactionListDTO> getRecentTransactionsByNameType(@RequestParam String name,@RequestParam String type){
+        return  null;
+    }
+    @GetMapping("/getTransactionInfoByTxid")
+    public TransactionInfoDTO getTransactionInfoByTxid(@RequestParam String txid){
+        return null;
+
+    }
+    @GetMapping("/getTransactionInfoByTxhash")
+    public TransactionInfoDTO getTransactionInfoByTxhash(@RequestParam String txhash){
+        return  null;
+    }
+
+
+}
