a717eb91d43679140baae59862ef356497e4165f
1464770973 <1464770973@qq.com>
2019-04-18 20:30:05 +0800

    add propertis

--- src/main/java/io/hzg/demo1/service/impl/MiscServiceImpl.java
+++ src/main/java/io/hzg/demo1/service/impl/MiscServiceImpl.java
@@ -1,11 +1,23 @@
 package io.hzg.demo1.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.hzg.demo1.api.BitcoinApi;
+import io.hzg.demo1.dao.BlockMapper;
+import io.hzg.demo1.po.Block;
 import io.hzg.demo1.service.MiscService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class MiscServiceImpl implements MiscService {
+    @Autowired
+    private BitcoinApi bitcoinApi;
+    @Autowired
+    private BlockMapper blockMapper;
     @Async
     @Override
     public void importFromHeight(Integer blockHeight, Boolean isClean) {
@@ -16,6 +28,33 @@ public class MiscServiceImpl implements MiscService {
     @Async
     @Override
     public void importFromHash(String blockHash, Boolean isClean) {
+
+        if (isClean){
+            blockMapper.truncate();
+        }
+
+        String temphash = blockHash;
+
+        while (temphash != null && !temphash.isEmpty()){
+            JSONObject blockOrigin = bitcoinApi.getBlock(temphash);
+            Block block = new Block();
+            block.setBlockhash(blockOrigin.getString("hash"));
+            block.setBlockchainId(2);
+            block.setHeight(blockOrigin.getInteger("height"));
+            Long time = blockOrigin.getLong("time");
+            Date date = new Date(time * 1000);
+            block.setTime(date);
+            JSONArray tx = blockOrigin.getJSONArray("tx");
+            block.setTxSize(tx.size());
+            block.setSizeOnDisk(blockOrigin.getLong("size"));
+            block.setDifficulty(blockOrigin.getDouble("difficulty"));
+            block.setPrevBlockhash(blockOrigin.getString("previousblockhash"));
+            block.setNextBlockhash(blockOrigin.getString("nextblockhash"));
+            block.setMerkleRoot(blockOrigin.getString("merkleroot"));
+            blockMapper.insert(block);
+
+            temphash = blockOrigin.getString("nextblockhash");
+        }
 
     }
 }
